4f1a3b24f0510b2512f83716c7b7d88ba841b1cb
Shiwank <shiwannnnk@gmail.com>
2025-04-22 00:19:22 +0530

    created first database

--- College.sql
+++ College.sql
@@ -1,1 +1,14 @@
-create database temp2;
+CREATE DATABASE college;
+
+USE college;
+
+CREATE TABLE student(
+	id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT NOT NULL
+);
+
+INSERT INTO student VALUES(1,"Shiwank",21);
+INSERT INTO student VALUES(2,"Tarang",20);
+
+SELECT * FROM student;
