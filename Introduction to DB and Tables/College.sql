90fd3da2aed73640f070a3c03e4160c801059aa0
Shiwank <shiwannnnk@gmail.com>
2025-04-22 21:27:45 +0530

    learned tables and DB queries

--- Introduction to DB and Tables/College.sql
+++ Introduction to DB and Tables/College.sql
@@ -1,5 +1,8 @@
 CREATE DATABASE college;
 
+-- CREATE DATABASE IF NOT EXISTS college;
+-- DROP DATABASE IF EXISTS school;
+
 USE college;
 
 CREATE TABLE student(
@@ -8,7 +11,17 @@ CREATE TABLE student(
     age INT NOT NULL
 );
 
-INSERT INTO student VALUES(1,"Shiwank",21);
-INSERT INTO student VALUES(2,"Tarang",20);
+INSERT INTO student(id, name, age) VALUES(1,"Shiwank",21);
+INSERT INTO student(id, name, age) VALUES(2,"Tarang",20);
+-- this student(defines the order) and it is optional, but the order should be same as you have define
+INSERT INTO student
+(id, name, age)
+VALUES
+(3,"Dev",22),
+(4,"Ashmit",21),
+(5,"Gaurav",20);
 
 SELECT * FROM student;
+
+SHOW TABLES;
+SHOW DATABASES;
